24bb3e7bc7b00bf47cd377c5786b95caf15ecbb4
Farzan V V M <m1093361@mindtree.com>
2024-01-09 21:10:32 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
+import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { EffectsModule } from "@ngrx/effects";
+import { StoreModule } from "@ngrx/store";
+import { AppRoutingModule } from "@spartacus/storefront";
 import { AppComponent } from './app.component';
+import { SpartacusModule } from './spartacus/spartacus.module';
+import { SearchBoxModule,MediaModule,ProductImagesModule} from '@spartacus/storefront';
+
+
+// import {StoreFrontModule}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([]),
+    SpartacusModule,
+    SearchBoxModule,
+    MediaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
